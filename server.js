7f212481bdeb0177929f2bf970957602b21eb8fa
Bull-Butcher <markjamesbutcher@gmail.com>
2019-07-29 15:56:08 +1000

    unresolved emit issue

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
 server.listen(process.env.PORT || 3000);
-console.log('Server running');
+console.log('Server running on 3000');
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -14,25 +14,26 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
     connections.push(socket);
-    console.log('Connection %s socket connected', connections.length)
+    console.log('Connection: %s socket connected', connections.length)
 
     // Disconnect
     socket.on('disconnect', function(data) {
             users.splice(users.indexOf(socket.username), 1);
+            updateUsernames();
             connections.splice(connections.indexOf(socket), 1);
-            console.log('Disconnected %s sockets connected', connections.length)
+            console.log('Disconnected: %s sockets connected', connections.length)
         })
         // Send Message
-
     socket.on('send message', function(data) {
         console.log(data)
         io.socket.emit('new message', { msg: data, user: socket.username });
     })
-
+    // New User
     socket.on('new user', function(data, callback) {
         callback(true);
         socket.username = data;
         user.push(socket.username);
+        console.log(socket.username)
         updateUsernames();
     })
 
